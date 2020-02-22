337dc5e5351409e761781af7066ea9a49bfe4784
Anwesh43 <anweshthecool0@gmail.com>
2020-02-22 22:54:54 +0530

    adding app and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedmulticoncentricarcview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmulticoncentricarcview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedmulticoncentricarcview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.multiconcentricarcview.MultiConcentricArcView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MultiConcentricArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
