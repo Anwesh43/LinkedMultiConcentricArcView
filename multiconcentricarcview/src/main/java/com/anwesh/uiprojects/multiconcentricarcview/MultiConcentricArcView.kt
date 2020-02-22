35c324d71bbf854e8ec9c47b2aa0d79cf69ddbf6
Anwesh43 <anweshthecool0@gmail.com>
2020-02-22 21:08:54 +0530

    adding MultiConcentricArcView class

--- multiconcentricarcview/src/main/java/com/anwesh/uiprojects/multiconcentricarcview/MultiConcentricArcView.kt
+++ multiconcentricarcview/src/main/java/com/anwesh/uiprojects/multiconcentricarcview/MultiConcentricArcView.kt
@@ -50,3 +50,19 @@ fun Canvas.drawMCANode(i : Int, scale : Float, paint : Paint) {
     drawMultiConcentricArcs(scale, size, paint)
     restore()
 }
+
+class MultiConcentricArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
