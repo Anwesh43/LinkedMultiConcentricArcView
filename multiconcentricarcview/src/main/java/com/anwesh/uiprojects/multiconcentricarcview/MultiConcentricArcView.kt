58e85b9518e5b2fd83674e25fd1062b4af7ae122
Anwesh43 <anweshthecool0@gmail.com>
2020-02-22 22:32:18 +0530

    adding MutliConcentricArc class

--- multiconcentricarcview/src/main/java/com/anwesh/uiprojects/multiconcentricarcview/MultiConcentricArcView.kt
+++ multiconcentricarcview/src/main/java/com/anwesh/uiprojects/multiconcentricarcview/MultiConcentricArcView.kt
@@ -154,4 +154,27 @@ class MultiConcentricArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class MultiConcentricArc(var i : Int) {
+
+        private var curr : MCANode = MCANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
